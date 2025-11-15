1133f2252723808f51f9b1093dd41811e5a99d25
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:00:53 +0100

    feat: add Doctor class

--- src/main/java/hu/herczeg/medicalassistantjava/model/Doctor.java
+++ src/main/java/hu/herczeg/medicalassistantjava/model/Doctor.java
@@ -1,4 +1,37 @@
 package hu.herczeg.medicalassistantjava.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "DOCTORS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Doctor {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = true, length = 100)
+    private String address;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 100)
+    private String email;
+
+    private String PasswordHash;
+
 }
