6ab7517521dbeeac3766a718012896897f4f54f2
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:54:45 +0100

    feat: add doctor login dto

--- src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/LoginDoctorDto.java
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/LoginDoctorDto.java
@@ -1,25 +1,13 @@
 package hu.herczeg.medicalassistantjava.dto.doctordtos;
 
-import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.*;
 
-@Builder
-@Getter
-@Setter
-@AllArgsConstructor
-public class RegisterDoctorDto{
-    @NotBlank(message = "Name cannot be blank")
-    @Pattern(regexp = "^[A-Za-zÀ-ž\\ \\s'-]{2,50}$", message = "Name should only contain letters, spaces or hypens!")
-    public String Name;
-    @Nullable
-    public String Address;
-    public String Phone;
+public class LoginDoctorDto{
     @Email
     public String Email;
+    @NotBlank
     @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Password should be atleast 8 characters long, have atleast 1 uppercase 1 lowercase and a special character")
     public String Password;
 }
-
