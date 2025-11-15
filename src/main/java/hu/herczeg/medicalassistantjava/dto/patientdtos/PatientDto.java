b10d8701ca438f185522c61a57d83ac37d43fb86
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:36:13 +0100

    feat: add patient dto, no validation since it's only being sent to the frontend

---
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/patientdtos/PatientDto.java
@@ -0,0 +1,13 @@
+package hu.herczeg.medicalassistantjava.dto.patientdtos;
+
+import java.time.LocalDateTime;
+
+public class PatientDto {
+    public Long Id;
+    public String Name;
+    public String Address;
+    public String Taj;
+    public String Complaints;
+    public LocalDateTime TimeOfAdmission;
+}
+
