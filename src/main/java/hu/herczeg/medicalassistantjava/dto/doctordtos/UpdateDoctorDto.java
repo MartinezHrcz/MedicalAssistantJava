9c8b9302c191ac3c33f6d2bbc63114ad0220faec
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:51:49 +0100

    feat: add doctor update dto

---
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/UpdateDoctorDto.java
@@ -0,0 +1,22 @@
+package hu.herczeg.medicalassistantjava.dto.doctordtos;
+
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+public class UpdateDoctorDto{
+    @NotBlank(message = "Name cannot be blank")
+    @Pattern(regexp = "^[A-Za-zÀ-ž\\ \\s'-]{2,50}$", message = "Name should only contain letters, spaces or hypens!")
+    public String Name;
+    @Nullable
+    public String Address;
+    public String Phone;
+    @Email
+    public String Email;
+}
