f873bd0faecd3385afde742d4d3d9e24e70c0dd7
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:44:46 +0100

    feat: add lombok getter, builder, allargscons annotations to dto classes

--- src/main/java/hu/herczeg/medicalassistantjava/dto/patientdtos/PatientDto.java
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/patientdtos/PatientDto.java
@@ -1,7 +1,16 @@
 package hu.herczeg.medicalassistantjava.dto.patientdtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class PatientDto {
     public Long Id;
     public String Name;
--- src/main/java/hu/herczeg/medicalassistantjava/dto/patientdtos/PatientMedicationDto.java
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/patientdtos/PatientMedicationDto.java
@@ -1,9 +1,17 @@
 package hu.herczeg.medicalassistantjava.dto.patientdtos;
 
 import hu.herczeg.medicalassistantjava.model.Medication;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class PatientMedicationDto{
     public Long Id;
     public String Taj;
