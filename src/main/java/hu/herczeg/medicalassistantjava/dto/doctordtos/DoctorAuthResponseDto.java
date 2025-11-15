7734c46a744d98ce303e23d9f1df57ad01bc813f
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:56:06 +0100

    feat: add doctor AuthRespone dto

---
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/DoctorAuthResponseDto.java
@@ -0,0 +1,8 @@
+package hu.herczeg.medicalassistantjava.dto.doctordtos;
+
+import hu.herczeg.medicalassistantjava.model.Doctor;
+
+public class DoctorAuthResponseDto{
+    public DoctorDto doctorDto;
+    public String Token;
+}
--- src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/LoginDoctorDto.java
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/LoginDoctorDto.java
@@ -11,3 +11,4 @@ public class LoginDoctorDto{
     @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Password should be atleast 8 characters long, have atleast 1 uppercase 1 lowercase and a special character")
     public String Password;
 }
+
