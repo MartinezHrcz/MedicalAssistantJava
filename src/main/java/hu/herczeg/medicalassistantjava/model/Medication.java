cd25dbef9406b9c48c537da4e2549b6bfae49cef
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:20:39 +0100

    feat: add medication entity

--- src/main/java/hu/herczeg/medicalassistantjava/model/Medication.java
+++ src/main/java/hu/herczeg/medicalassistantjava/model/Medication.java
@@ -1,4 +1,29 @@
 package hu.herczeg.medicalassistantjava.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.id.GUIDGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "MEDS")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Medication {
+    @Id
+    private UUID id;
+
+    private String title;
+
+    private String description;
+
+    @OneToOne
+    @JoinColumn(name = "patient_id")
+    private Patient patient;
 }
