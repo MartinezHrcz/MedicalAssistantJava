221938b566d822a04712310d3d6b0d0fbfb330e3
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:38:03 +0100

    feat: add patient login dto

---
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/patientdtos/PatientLoginDto.java
@@ -0,0 +1,11 @@
+package hu.herczeg.medicalassistantjava.dto.patientdtos;
+
+import jakarta.validation.constraints.Pattern;
+
+public class PatientLoginDto{
+    @Pattern(regexp = "^\\d{3}-\\d{3}-\\d{3}$", message = "TAJ must be in 000-000-000 format!")
+    public String Taj;
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Password should be atleast 8 characters long, have atleast 1 uppercase 1 lowercase and a special character")
+    public String Password;
+}
+
