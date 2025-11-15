4e2830aa5c561e435c8d274e37d62fc9e2017a2e
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-15 20:58:18 +0100

    feat: add common password update dto

---
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/common/PasswordUpdateDto.java
@@ -0,0 +1,16 @@
+package hu.herczeg.medicalassistantjava.dto.common;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PasswordUpdateDto {
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Password should be atleast 8 characters long, have atleast 1 uppercase 1 lowercase and a special character")
+    public String oldPassword;
+    @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", message = "Password should be atleast 8 characters long, have atleast 1 uppercase 1 lowercase and a special character")
+    public String newPassword;
+}
--- src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/DoctorAuthResponseDto.java
+++ src/main/java/hu/herczeg/medicalassistantjava/dto/doctordtos/DoctorAuthResponseDto.java
@@ -1,7 +1,15 @@
 package hu.herczeg.medicalassistantjava.dto.doctordtos;
 
 import hu.herczeg.medicalassistantjava.model.Doctor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class DoctorAuthResponseDto{
     public DoctorDto doctorDto;
     public String Token;
