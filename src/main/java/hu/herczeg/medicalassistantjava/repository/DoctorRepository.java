a5b4c913557900475dd1e488d4098066a8f68615
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-16 16:49:55 +0100

    chore: make repos public

--- src/main/java/hu/herczeg/medicalassistantjava/repository/DoctorRepository.java
+++ src/main/java/hu/herczeg/medicalassistantjava/repository/DoctorRepository.java
@@ -1,9 +1,18 @@
 package hu.herczeg.medicalassistantjava.repository;
 
 import hu.herczeg.medicalassistantjava.model.Doctor;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    List<Doctor> findAllByNameContainingIgnoreCase(String name);
+
+    boolean findByEmailEquals(String email);
+
+    Doctor getDoctorByEmail(@Email String email);
+
+    Doctor findByEmailEqualsIgnoreCase(String email);
 }
--- src/main/java/hu/herczeg/medicalassistantjava/repository/PatientRepository.java
+++ src/main/java/hu/herczeg/medicalassistantjava/repository/PatientRepository.java
@@ -1,9 +1,14 @@
 package hu.herczeg.medicalassistantjava.repository;
 
+import hu.herczeg.medicalassistantjava.model.Doctor;
 import hu.herczeg.medicalassistantjava.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findAllByDoctor(Doctor doctor);
+
+    Patient findByTaj(String taj);
 }
