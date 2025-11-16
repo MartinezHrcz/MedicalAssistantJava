14a688397c669097a188eec97ccf0913dc621e0f
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-16 16:12:08 +0100

    chore: add patient mapper

---
+++ src/main/java/hu/herczeg/medicalassistantjava/mappers/PatientMapper.java
@@ -0,0 +1,23 @@
+package hu.herczeg.medicalassistantjava.mappers;
+
+import hu.herczeg.medicalassistantjava.dto.patientdtos.PatientDto;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.PatientMedicationDto;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.RegisterPatientDto;
+import hu.herczeg.medicalassistantjava.model.Patient;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PatientMapper {
+    PatientDto toDto(Patient patient);
+
+    Patient toEntity(PatientDto patientDto);
+
+    Patient toEntity(RegisterPatientDto dto);
+
+    PatientMedicationDto toMedicationDto(Patient patient);
+
+    List<PatientDto> toDtos(List<Patient> patients);
+}
