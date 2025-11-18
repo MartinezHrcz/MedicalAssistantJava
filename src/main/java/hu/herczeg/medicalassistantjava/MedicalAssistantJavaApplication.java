7ef830fda08115e05a374909421550c793a5c9d7
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-18 22:05:22 +0100

    fix: repository thinking that dto is an entity fixed by limiting entity scan

--- src/main/java/hu/herczeg/medicalassistantjava/MedicalAssistantJavaApplication.java
+++ src/main/java/hu/herczeg/medicalassistantjava/MedicalAssistantJavaApplication.java
@@ -2,8 +2,10 @@ package hu.herczeg.medicalassistantjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "hu.herczeg.medicalassistantjava.model")
 public class MedicalAssistantJavaApplication {
 
     public static void main(String[] args) {
