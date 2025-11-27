3ba7cc36e15d139c71feeb5acf3d8b1867b3cf77
Herczeg Martinez <herczegmartinez@gmail.com>
2025-11-27 17:45:06 +0100

    chore: patient controller fixed styling

--- src/main/java/hu/herczeg/medicalassistantjava/controller/PatientController.java
+++ src/main/java/hu/herczeg/medicalassistantjava/controller/PatientController.java
@@ -1,10 +1,22 @@
 package hu.herczeg.medicalassistantjava.controller;
 
 import hu.herczeg.medicalassistantjava.dto.common.PasswordUpdateDto;
-import hu.herczeg.medicalassistantjava.dto.patientdtos.*;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.PatientDto;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.PatientMedicationDto;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.PatientLoginDto;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.RegisterPatientDto;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.PatientAuthResponseDto;
+import hu.herczeg.medicalassistantjava.dto.patientdtos.UpdatePatientDto;
 import hu.herczeg.medicalassistantjava.service.interfaces.PatientService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -19,98 +31,86 @@ public class PatientController {
     }
 
     @GetMapping
-    public ResponseEntity<List<PatientDto>> getAll(){
+    public ResponseEntity<List<PatientDto>> getAll() {
         return ResponseEntity.ok(patientService.GetAllPatients());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<PatientDto> getById(@PathVariable("id") Long id){
+    public ResponseEntity<PatientDto> getById(@PathVariable("id") Long id) {
         try {
             return ResponseEntity.ok(patientService.GetPatientById(id));
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
     }
 
     @GetMapping("/byname/{name}")
-    public ResponseEntity<List<PatientDto>> getByName(@PathVariable("name") String name){
+    public ResponseEntity<List<PatientDto>> getByName(@PathVariable("name") String name) {
         return ResponseEntity.ok(patientService.GetPatientByName(name));
     }
 
     @GetMapping("/bytaj/{taj}")
-    public ResponseEntity<PatientDto> getBytaj(@PathVariable("taj") String taj){
+    public ResponseEntity<PatientDto> getBytaj(@PathVariable("taj") String taj) {
         try {
             return ResponseEntity.ok(patientService.GetPatientByTaj(taj));
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
     }
 
     @PostMapping
-    public ResponseEntity<PatientDto> create(@RequestBody RegisterPatientDto registerPatientDto){
-        try{
+    public ResponseEntity<PatientDto> create(@RequestBody RegisterPatientDto registerPatientDto) {
+        try {
             return ResponseEntity.ok(patientService.CreatePatient(registerPatientDto));
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<PatientDto> update(@PathVariable long id,@RequestBody UpdatePatientDto updatePatientDto){
+    public ResponseEntity<PatientDto> update(@PathVariable long id,@RequestBody UpdatePatientDto updatePatientDto) {
         try {
             return ResponseEntity.ok(patientService.UpdatePatient(id,updatePatientDto));
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @PutMapping("pwdupdate/{id}")
-    public ResponseEntity  updatePwd(@PathVariable long id, @RequestBody PasswordUpdateDto dto){
-        try{
-            return patientService.UpdatePatientPassword(id, dto) ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
-        }
-        catch(Exception e){
+    public ResponseEntity updatePwd(@PathVariable long id, @RequestBody PasswordUpdateDto dto) {
+        try {
+            return patientService.UpdatePatientPassword(id, dto)
+                    ? ResponseEntity.ok().build() : ResponseEntity.badRequest().build();
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @DeleteMapping("{id}")
-    public ResponseEntity delete(@PathVariable long id){
+    public ResponseEntity delete(@PathVariable long id) {
         try {
             patientService.DeletePatient(id);
             return ResponseEntity.ok().build();
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @PostMapping("login")
-    public ResponseEntity<PatientAuthResponseDto> login(@RequestBody PatientLoginDto dto){
-        try
-        {
+    public ResponseEntity<PatientAuthResponseDto> login(@RequestBody PatientLoginDto dto) {
+        try {
             return ResponseEntity.ok(patientService.LoginPatientAsync(dto));
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
 
     @GetMapping("/medication/{taj}")
-    public ResponseEntity<PatientMedicationDto> getMedication(@PathVariable String taj)
-    {
+    public ResponseEntity<PatientMedicationDto> getMedication(@PathVariable String taj) {
         try {
             return ResponseEntity.ok(patientService.GetPatientMedication(taj));
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().build();
         }
     }
-
-
-
 }
